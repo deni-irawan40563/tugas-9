96c448cb9a8bbd91a4a887d44bf321b61c7269b8
deni-irawan40563 <deni.irawan40563@gmail.com>
2020-06-18 10:38:57 +0700

    h

---
+++ Index.js
@@ -0,0 +1,29 @@
+//Soal no 1
+/*
+data base adalah kumpulan data yang di simpan secara sistematis untuk nantinya dapat digunakan dan di modifikasi.
+perbedaan data base Sql dan non Sqlyang paling utama adalah jika database Sql data di simpan dengan tabel sedangkan database non Sql data di simpan menggunakan dokumen dengan format JSON.
+Selain itu ini beberapa perbedaan lainnya :
+database Sql =
+1.data bersifat kaku karena di simpan dengan tabel
+2.data di simpan secara vertikal
+database no Sql =
+1.data tidak kaku dan efisien jika ingin menambahkan perubahan
+2.data di simpan secara horizontal
+*/
+
+//soal 2
+// Saya pernah belajar database di Progate dan telah selesai mempelajari fundamental penggunaan database menggunakan bahasa MySql
+
+//Soal 3
+/* 
+contoh query di MySql
+SELECT * 
+FROM customer
+WHERE negara = "Indonesia" ;
+*/
+
+//Soal 4
+/*
+contoh cara find all di mongo db
+db.note.find()
+*/
